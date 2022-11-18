4eabe7e8b800fee69e27a689af6ba607c36640ad
Octuffalo <108063393+Octuffalo@users.noreply.github.com>
2022-11-18 10:44:09 +0000

    Initial commit

---
+++ js/gaming.js
@@ -0,0 +1,21 @@
+const resultList = document.querySelector('#results');
+const url = 'https://content.guardianapis.com/search?q=gaming&api-key=';
+const apiKey = 'a59a883f-36dc-449a-85bc-2d45a95ed3ca'
+fetch(url + apiKey)
+.then((response) => response.json())
+.then((data) => {
+    data.response.results.forEach(function (value) {
+        console.log(value);
+        const articleElement = `<div class="col-md-4">
+                                    <div class="card mb-4">
+                                        <div class="card-body">
+                                            <h5 class="card-title">${value.webTitle}</h5>
+                                            <p class="card-text">${value.sectionName}</p>
+                                            <a target="_blank" href="${value.webUrl}" class="btn btn-primary">View Article</a>
+                                                </div>
+                                            </div>
+
+                                </div>`;
+        resultList.insertAdjacentHTML('beforeend', articleElement);
+    });
+});
---
+++ js/search.js
@@ -0,0 +1,26 @@
+const searchNews = (event) => {
+  event.preventDefault();
+  const keyword = document.querySelector("#keywords").value;
+  const url =
+    "https://content.guardianapis.com/search?q=" + keyword + "&api-key=";
+  const apiKey = "a59a883f-36dc-449a-85bc-2d45a95ed3ca";
+  const resultList = document.querySelector("#results");
+  resultList.innerHTML = "";
+  fetch(url + apiKey)
+    .then((response) => response.json())
+    .then((data) => {
+      data.response.results.forEach(function (value) {
+        console.log(value);
+        const articleElement = `<div class="col-md-4">
+                                        <div class="card mb-4">
+                                            <div class="card-body">
+                                                <h5 class="card-title">${value.webTitle}</h5>
+                                                <p class="card-text">${value.sectionName}</p>
+                                                <a target="_blank" href="${value.webUrl}" class="btn btn-primary">View Article</a>
+                                            </div>
+                                        </div>
+                                    <div>`;
+        resultList.insertAdjacentHTML("beforeend", articleElement);
+      });
+    });
+};
